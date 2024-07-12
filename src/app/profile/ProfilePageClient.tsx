2ce62ebe086588169d03b5887ed8c83463f644c4
alexaldearroyo <alexaldearroyo@gmail.com>
2024-07-12 11:11:14 +0200

    Update profile buttons styling

--- src/app/profile/ProfilePageClient.tsx
+++ src/app/profile/ProfilePageClient.tsx
@@ -139,12 +139,14 @@ export default function ProfilePageClient({
               </li>
             ))
           ) : (
-            <p>No hay posts aún.</p>
+            <p>No Posts Yet</p>
           )}
         </ul>
 
         <Link href={{ pathname: '/posts/new' }}>
-          <button className="button">Crear nuevo post</button>
+          <button className="mt-4 w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500">
+            Add new post
+          </button>
         </Link>
         <button
           onClick={() => (window.location.href = `/profile/edit`)}
