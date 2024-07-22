32fadddc6915f63c0f547a43100fefa0ed4624c5
alexaldearroyo <alexaldearroyo@gmail.com>
2024-07-22 13:30:02 +0200

    Update add comment button design

--- src/app/profile/[username]/public/Wall.tsx
+++ src/app/profile/[username]/public/Wall.tsx
@@ -223,7 +223,7 @@ export default function Wall({
         <div className="flex justify-center">
           <button
             type="submit"
-            className="w-0.5/2 h-10 px-4 bg-indigo-600 text-white rounded-md"
+            className="flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
           >
             Submit
           </button>
